71cbb631f4dc9ffa8f46a74464646ad5015caa1a
sarang323patil <sarang.patil323@gmail.com>
2021-10-21 22:53:08 +0530

    ready for deployment

--- Fintick/settings.py
+++ Fintick/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v4(znbv^afi!nwbo40_12$n28ybqv3yst92yg54jp5fgker1^i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','Fintick.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- Fintick/urls.py
+++ Fintick/urls.py
@@ -19,11 +19,16 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include
 from . import views
+from django.views.static import serve 
+from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
     path('display', views.display, name="display"),
+
+    
 ]
 
 urlpatterns += staticfiles_urlpatterns()
